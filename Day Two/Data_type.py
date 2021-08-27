c8c1abd7b9347a266b736f63eb05a19ce7f36795
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2021-08-27 10:57:59 +0600

    date_type

---
+++ Day Two/Data_type.py
@@ -0,0 +1,29 @@
+# Data Types
+# Built-in Data Types:
+# text: String (str)
+# Number: int, float, complex
+# Sequence: list, tuple, range
+# Mapping: dictionary (dict)
+# set : set, frozenset
+# boolean: boolean (bool)
+# binary : byte, bytearray, memoryview
+
+x_string = str("Hello World!")   # String
+x_int = 20                  # Int
+x_float = 3.1416            # Float
+x_complex = 3J              # Complex
+
+x_list = ["apple", 78, "cherry"] # List
+x_tuples = ("apple", "banana", "cherry") # tuple
+x_range = range(5)                       # range
+
+x_dict = { "name" : "Shishir", "description" : "iOS Developer"} # dictionary
+
+x_bools = True # Boolean
+x_bytes = bytes(5)
+x_bytesArray = bytearray(3)
+x_memoryview = memoryview(bytes(5))
+
+print (x_memoryview)
+print(5)
+print(x_complex)
