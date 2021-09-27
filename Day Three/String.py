bba313798e926a153c395f94b1bd5be52dc05eef
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2021-09-27 19:33:11 +0600

    string

--- Day Three/String.py
+++ Day Three/String.py
@@ -1,1 +1,15 @@
 var1 = 'Hello'
+var2 = "Hello"
+var3 = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+# 'hello' is the same as "hello"
+
+print(var1 + " " + var2)
+print(var3)
+
+# Strings are Arrays
+var4 = 'Hello world'
+print(var4[8])
