497a6fa0bae83b1264e383b2e491f713706dea32
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2021-09-27 21:34:14 +0600

    string

--- Day Three/String.py
+++ Day Three/String.py
@@ -6,19 +6,49 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 
+var5 = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
 # 'hello' is the same as "hello"
 
 print(var1 + " " + var2)
 print(var3)
+print(var5)
 
 # Strings are Arrays
 var4 = 'Hello world'
 
 # Python does not have a character data type, a single character is simply a string with a length of 1. Square brackets can be used to access elements of the string.
 
-print(var4[8])
+print(var4[6])
 
 # String Length
 # To get the length of a string, use the len() function.
 
 print(len(var4))
+# Modify Strings
+print(var4.upper())
+print(var4.lower())
+print(type(var4))
+
+# Check String
+
+var6 = "The best things in life are free!"
+
+print("life" in var6)
+
+# Check if NOT
+print("expensive" not in var6)
+
+
+print(var6[4:8])
+print(var6[:14])
+print(var6[4:])
+
+var7 = "       I am shishir "
+print(var7.strip())
+
+print(var7.replace("am", "was"))
+
+print(var7.split())
